c38196f4764af3600c01df886f3f9d95ff23dfe8
tyMind <tatul.01yeghiazaryan@gmail.com>
2020-12-06 18:33:03 +0400

    update matrix class c1

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,5 +1,5 @@
 #include "Matrix.h"
-#include <time.h>
+#include <ctime>
 #include <fstream>
 #include <sstream>
 
@@ -8,11 +8,19 @@ Matrix::Matrix(int row, int col):m_row(row), m_col(col){
     fillRandNums();    //initializes main matrix
 }
 
-std::vector<std::vector<int>> Matrix::getMatrix() {
+std::vector<std::vector<int>> Matrix::getMatrix() const{
     return m_matx;
 }
 
-std::vector<std::vector<int>> Matrix::getSubMatrix(int startRow, int startCol, int endRow, int endCol){
+int Matrix::getCol() const{
+    return m_col;
+}
+
+int Matrix::getRow() const {
+    return m_row;
+}
+
+std::vector<std::vector<int>> Matrix::getSubMatrix(int startRow, int startCol, int endRow, int endCol) const{
     std::vector<std::vector<int>>subMatrix;
     for(int i=startRow; i<endRow; i++){
         std::vector<int>rowLine;
@@ -24,9 +32,11 @@ std::vector<std::vector<int>> Matrix::getSubMatrix(int startRow, int startCol, i
     return subMatrix;
 }
 
-void Matrix::setMatrix(std::vector<std::vector<int>> outMatx) {
+void Matrix::setMatrix(const std::vector<std::vector<int>> &outMatx) {
     int rowSize=outMatx.size();
     int colSize=outMatx[0].size();
+    this->m_row=rowSize;
+    this->m_col=colSize;
 
     for(int i=0; i<rowSize; i++){
         m_matx.push_back(std::vector<int>());
@@ -57,31 +67,21 @@ void Matrix::fillRandNums() {
     for(int i=0; i<m_row; i++){
         m_matx.push_back(std::vector<int>(m_col));
         for(int j=0; j<m_col; j++){
-            num=rand()%2;
+            num=rand()%10;
             m_matx[i][j]=num;
         }
     }
 }
 
-void Matrix::writeNumOfMatrices(std::string fileName, int numOfMatrices) {
-//    int prevNumOfMatrices=0;
-//    std::ifstream inFile(fileName);
-//    if(!inFile.is_open()){
-//        std::cout<<"Couldn't open the file: "<<fileName<<'\n';
-//    }
-//    else{
-//        inFile>>prevNumOfMatrices;
-//    }
-
+void Matrix::writeNumOfMatrices(const std::string &fileName, int numOfMatrices) {
     std::fstream outFile;
     outFile.open(fileName, std::fstream::out);
     outFile<<numOfMatrices;
     outFile<<std::endl;
     outFile.close();
-//    inFile.close();
 }
 
-void Matrix::writeToFile(std::string fileName) {
+void Matrix::writeToFile(const std::string &fileName) {
     std::fstream outFile;
     outFile.open(fileName, std::fstream::out | std::fstream::app);
     outFile<<m_row<<" "<<m_col<<" ";
@@ -97,7 +97,7 @@ void Matrix::writeToFile(std::string fileName) {
     outFile.close();
 }
 
-std::vector<Matrix> Matrix::readFromFile(std::string fileName) {
+std::vector<Matrix> Matrix::readFromFile(const std::string &fileName) {
     std::ifstream inFile;
     inFile.open(fileName, std::fstream::in);
     std::string line;
@@ -108,25 +108,24 @@ std::vector<Matrix> Matrix::readFromFile(std::string fileName) {
     int rowInFile=1;
     int nextMatrixRow=2;    //next matrix row in file
     int value=0;
-    int getMatSize=0;
+    int getMatricesNum=0;
 
     std::getline(inFile, line);
     std::stringstream lineStream(line);
     if(rowInFile==1){
         rowInFile++;
         while(lineStream>>value){
-            getMatSize=value;
+            getMatricesNum=value;
         }
     }
 
-    std::vector<Matrix>matrices(getMatSize);
-//    std::vector<std::vector<std::vector<int>>>matrices(getMatSize);
+    std::vector<Matrix>matrices(getMatricesNum);
     int matrixIt=-1;
-    while(std::getline(inFile, line) && matrixIt<getMatSize){
+    while(std::getline(inFile, line) && matrixIt<getMatricesNum){
         std::vector<int>lineData;
         std::stringstream lineStream(line);
         static int colIt=0;
-        if(line!=""){
+        if(!line.empty()){
             if(rowInFile==nextMatrixRow){
                 int i=0;
                 while(lineStream>>value){
@@ -159,36 +158,37 @@ std::vector<Matrix> Matrix::readFromFile(std::string fileName) {
     return matrices;
 }
 
-Matrix Matrix::applyFilter(Matrix b) {
+Matrix Matrix::applyFilter(const Matrix &b) {
     Matrix retMatrix;
+
     int retMatRowSize=(this->m_row)-(b.m_row)+1;
     int retMatColSize=(this->m_col)-(b.m_col)+1;
+    if(retMatRowSize==-1 && retMatColSize==-1){
+        retMatRowSize++;
+        retMatColSize++;
+    }
     retMatrix.setRowColVal(retMatRowSize, retMatColSize);
-    retMatrix.m_matx=std::vector<std::vector<int>>(retMatRowSize);    //maybe resizing would be better
+    retMatrix.m_matx=std::vector<std::vector<int>>(retMatRowSize);
     auto filterMatrix=b.getMatrix();
     for(int i=0; i<retMatRowSize; i++){
         for(int j=0; j<retMatColSize; j++){
-            int count=0;
+            int result=0;
             auto subMatrix=getSubMatrix(i, j, i+b.m_row, j+b.m_col);
             for(int row=0; row<b.m_row; row++){
                 for(int col=0; col<b.m_col; col++){
-                    if(subMatrix[row][col]==1 && subMatrix[row][col]==filterMatrix[row][col]){
-                        count++;
-                    }
+                    result+=subMatrix[row][col]*filterMatrix[row][col];
                 }
             }
-        retMatrix.m_matx[i].push_back(count);
+          retMatrix.m_matx[i].push_back(result);
         }
     }
     return retMatrix;
 }
 
-Matrix Matrix::dotProduct(std::vector<std::vector<int>>subMatrix, Matrix b){
-    auto filterMatrix=b.getMatrix();
-//    Matrix returnMatx(filterMatrix.size(), filterMatrix[0].size());
+Matrix Matrix::dotProduct(const std::vector<std::vector<int>>&subMatrix, const Matrix &b){
+    const auto filterMatrix=b.getMatrix();
     Matrix returnMatx;
     returnMatx.setRowColVal(filterMatrix.size(), filterMatrix[0].size());
-    int sum=0;
     std::vector<std::vector<int>>outMatx(subMatrix.size());
     for(int i=0; i<subMatrix[0].size(); i++){
         for(int j=0; j<filterMatrix.size(); j++){
--- Matrix.h
+++ Matrix.h
@@ -11,29 +11,28 @@ private:
     std::vector<std::vector<int>>m_matx;
 
 public:
-    Matrix(){}
+    Matrix():m_row(0), m_col(0){}
     Matrix(int row, int col);
-    std::vector<std::vector<int>> getSubMatrix(int startRow, int startCol, int endRow, int endCol);
-    std::vector<std::vector<int>> getMatrix();
-    int getRow(){
-        return m_row;
-    }
-    int getCol(){
-        return m_col;
-    }
-    void setMatrix(std::vector<std::vector<int>>outMatx);
-    void setRowColVal(int row, int col);
+    std::vector<std::vector<int>> getSubMatrix(int startRow, int startCol, int endRow, int endCol) const; //seperate the submatrix from
+                                                                                                    //the main matrix on which the
+                                                                                                    //kernel must run(in each step
+                                                                                                    //there are different submatrices)
+    std::vector<std::vector<int>> getMatrix() const;  //get the matrix from matrix object
+    int getRow() const;
+    int getCol() const;
+
+    void setMatrix(const std::vector<std::vector<int>>&outMatx);   //"copy" outMatx matrix into Matrix object's m_matx
+    void setRowColVal(int row, int col);    //set the number of rows and columns in matrix object
     void printMatrix();
 
-    void fillRandNums(); //push random numbers into m_matx
+    void fillRandNums(); //push random numbers into m_matx, those numbers can be from the range 0 to 9
 
-    static void writeNumOfMatrices(std::string fileName, int numOfMatrices);    //write how many matrices you want to insert into the file
-    void writeToFile(std::string fileName);
-    static std::vector<Matrix> readFromFile(std::string fileNam);    //read all matrixes from file and push it to the vector
-//    void updateMatrix();
+    static void writeNumOfMatrices(const std::string &fileName, int numOfMatrices);    //write how many matrices you want to insert into the file
+    void writeToFile(const std::string &fileName); //write a matrix to a file
+    static std::vector<Matrix> readFromFile(const std::string &fileName);    //read all matrices from file and push it to the vector
 
-    Matrix applyFilter(Matrix b);
-    Matrix dotProduct(std::vector<std::vector<int>>subMatrix, Matrix b);
+    Matrix applyFilter(const Matrix &b);   //apply filter on a Matrix object
+    Matrix dotProduct(const std::vector<std::vector<int>>&subMatrix, const Matrix &b);
 
 };
 
